d7caade9ef8846c30c4cef23fb76ccf377e1347c
nikhilmopidevi <nikhilkumarmopidevi@gmail.com>
2017-04-03 09:22:25 -0400

    Added Stack Implementation

---
+++ Stack/Stack.java
@@ -0,0 +1,60 @@
+package com.datastructures.Stack;
+
+/**
+ * Author: Nikhil Kumar Mopidevi
+ * Date: 4/3/17
+ */
+public class Stack<E> {
+    private E array[];
+    private int capacity;
+    private int currentIndex;
+
+    //default constructor
+    public Stack() {
+        capacity = 16;
+        currentIndex = -1;
+        array = (E[])new Object[16];
+    }
+
+    //push an element into the stack
+    public boolean push(E element) {
+        if(!isFull()) {
+            currentIndex++;
+            array[currentIndex] = element;
+        }
+        else return false;
+        return true;
+    }
+
+    //pop an element from the top of the stack
+    public E pop() {
+        if(!isEmpty()) {
+            E element = array[currentIndex];
+            currentIndex--;
+            return element;
+        }
+        else return null;
+    }
+
+    //returns the element from the top of the stack, without removing it
+    public E peek() {
+        if(!isEmpty()) {
+            return array[currentIndex];
+        }
+        else return null;
+    }
+
+    //checks of stack is empty
+    public boolean isEmpty() {
+        if(currentIndex == -1)
+            return true;
+        else return false;
+    }
+
+    //checks if stack is full
+    public boolean isFull() {
+        if(currentIndex == capacity-1)
+            return true;
+        else return false;
+    }
+}
---
+++ Stack/StackTest.java
@@ -0,0 +1,38 @@
+package com.datastructures.Stack;
+
+/**
+ * Author: Nikhil Kumar Mopidevi
+ * Date: 4/3/17
+ */
+public class StackTest {
+    public static void main(String[] args) {
+
+        int nums[] = {1,10,9,3,4,2,8,5};
+
+        Stack stack = new Stack();
+
+        //adding all elements of nums[] to the stack
+        for(int i=0;i<nums.length;i++) {
+            stack.push(nums[i]);
+        }
+
+        //prints the element at the top of the stack
+        System.out.print("\nCurrent element at the top of the stack: ");
+        System.out.println(stack.peek());
+
+        System.out.println("\nAdding element 6 to the stack.");
+        stack.push(6); //adds the element 6 to the stack
+
+        //prints the element at the top of the stack
+        System.out.print("\nCurrent element at the top of the stack: ");
+        System.out.println(stack.peek());
+
+        System.out.println("\nRemoving first two elements of the stack.");
+        System.out.println("\nThe removed element is: " + stack.pop()); //removes the element at the top of the stack
+        System.out.println("\nThe removed element is: " + stack.pop()); //removes the element at the top of the stack
+
+        //prints the element at the top of the stack
+        System.out.print("\nCurrent element at the top of the stack: ");
+        System.out.println(stack.peek());
+    }
+}
